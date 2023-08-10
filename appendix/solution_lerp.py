143d7839249681be9d339f99f92ccfe76b73c984
Théo Martinez <theo.martinez-fouche@laas.fr>
2023-08-10 15:40:18 +0200

    fix missing imports, remove unnecessary generate.py file

--- appendix/solution_lerp.py
+++ appendix/solution_lerp.py
@@ -1,10 +1,13 @@
 import time
+
 import numpy as np
 
-p0 = np.random.rand(3)
-p1 = np.random.rand(3)
 
-for t in np.arange(0, 1, .01):
-    p = p0 * (1 - t) + p1 * t
-    viz.applyConfiguration('world/box', list(p) + list(quat.coeffs()))
-    time.sleep(.01)
+def change_box_pose_in_viz(viz, quat):
+    p0 = np.random.rand(3)
+    p1 = np.random.rand(3)
+
+    for t in np.arange(0, 1, 0.01):
+        p = p0 * (1 - t) + p1 * t
+        viz.applyConfiguration("world/box", list(p) + list(quat.coeffs()))
+        time.sleep(0.01)
--- appendix/solution_slerp.py
+++ appendix/solution_slerp.py
@@ -1,15 +1,23 @@
 from time import sleep
+
 import numpy as np
 from numpy.linalg import norm
 from pinocchio import SE3, AngleAxis, Quaternion
 
+from utils.meshcat_viewer_wrapper import MeshcatVisualizer
+
+viz = MeshcatVisualizer()
+
+
 def _lerp(p0, p1, t):
     return (1 - t) * p0 + t * p1
 
+
 def slerp(q0, q1, t):
-    assert (t >= 0 and t <= 1)
+    assert t >= 0 and t <= 1
     a = AngleAxis(q0.inverse() * q1)
     return Quaternion(AngleAxis(a.angle * t, a.axis))
+
 
 def nlerp(q0, q1, t):
     q0 = q0.coeffs()
@@ -18,14 +26,14 @@ def nlerp(q0, q1, t):
     lerp /= norm(lerp)
     return Quaternion(lerp[3], *list(lerp[:3]))
 
+
 q0 = Quaternion(SE3.Random().rotation)
 q1 = Quaternion(SE3.Random().rotation)
-viz.applyConfiguration('world/box', [0, 0, 0] + list(q0.coeffs()))
-sleep(.1)
-for t in np.arange(0, 1, .01):
+viz.applyConfiguration("world/box", [0, 0, 0] + list(q0.coeffs()))
+sleep(0.1)
+for t in np.arange(0, 1, 0.01):
     q = nlerp(q0, q1, t)
-    viz.applyConfiguration('world/box', [0, 0, 0] + list(q.coeffs()))
-    sleep(.01)
-sleep(.1)
-viz.applyConfiguration('world/box', [0, 0, 0] + list(q1.coeffs()))
-
+    viz.applyConfiguration("world/box", [0, 0, 0] + list(q.coeffs()))
+    sleep(0.01)
+sleep(0.1)
+viz.applyConfiguration("world/box", [0, 0, 0] + list(q1.coeffs()))
